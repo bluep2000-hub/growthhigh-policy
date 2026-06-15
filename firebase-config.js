4a115ccf8ce856ee83f4fc5b473ca2083586fd0d
bluep2000-hub <bluep2000@gmail.com>
2026-06-16 07:42:23 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// GROWTH'HIGH 재생목록 - Firebase 설정
+export const firebaseConfig = {
+  apiKey: "AIzaSyA3KKa6nMaqi7goiNoMbExYnbaTtjCjIGI",
+  authDomain: "growthhigh-playlist.firebaseapp.com",
+  projectId: "growthhigh-playlist",
+  storageBucket: "growthhigh-playlist.firebasestorage.app",
+  messagingSenderId: "393322306234",
+  appId: "1:393322306234:web:69f9209d50e9507fff8083"
+};
+// 편집 암호 (full.html에서 담기/빼기 시 1회 입력)
+export const EDIT_PASSCODE = "qw241563";
